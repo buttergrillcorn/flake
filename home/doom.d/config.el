47a0781caa6833b7f2de91a12753fd40db1e7497
buttergrillcorn <jamesyuli@outlook.com>
2024-01-05 18:58:27 +0000

    mod

--- home/doom.d/config.el
+++ home/doom.d/config.el
@@ -1,3 +1,4 @@
+;;; This dotfile is managed using dotdrop
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
@@ -20,10 +21,11 @@
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
-;;
-(setq doom-font (font-spec :family "Maple Mono SC NF" :size 12 :weight 'regular))
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
+
+;; (setq doom-font (font-spec :family "Sarasa Term SC Nerd" :size 15 :weight 'Semibold)
+;;       doom-variable-pitch-font (font-spec :family "Sarasa Term SC Nerd" :size 15)
+;;       doom-unicode-font (font-spec :family "Symbols Nerd Font Mono" :size 16))
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -32,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
+(setq doom-theme 'doom-tomorrow-night)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -42,6 +44,28 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+;; Org Capture
+(after! org
+  (setq org-capture-templates
+        `(("t" "Todo" entry (file "~/org/todo.org")
+           "* TODO %U %?" :empty-lines-after 1)
+          ("i" "Inbox" entry (file "~/org/inbox.org")
+           "* %t %?" :empty-lines-after 1)
+          ("w" "Work" entry (file "~/org/work.org")
+           "* %t %?  %(org-set-tags \"work\")" :empty-lines-after 1)
+          ("j" "Journal" entry (file+datetree "~/org/journal.org")
+           "* %?" :jump-to-captured t :time-prompt t)
+          )))
+
+;; Org Babel
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(
+   (emacs-lisp . t)
+   (python . t)
+   (conf-toml . t)
+   (rust . t)
+   ))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -74,3 +98,63 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Load external files
+(load! "font.el")
+
+;; Keybinds
+(map! "C-h" 'switch-to-prev-buffer)
+(map! "C-l" 'switch-to-next-buffer)
+(map! "C-S-l" 'recenter-top-bottom)
+
+;; Which-key
+;; (setq which-key-idle-delay 0.2)
+(setq auto-save-visited-interval 15)
+(auto-save-visited-mode +1)
+
+;; Org auto list
+(add-hook 'org-mode-hook
+          (lambda () (org-autolist-mode)))
+
+;; Projects
+(projectile-add-known-project "~/org")
+(projectile-add-known-project "~/dot")
+
+;; Shell
+(setq shell-file-name (executable-find "bash"))
+(setq-default vterm-shell (executable-find "fish"))
+(setq-default explicit-shell-file-name (executable-find "fish"))
+
+;; dimmer
+(use-package! dimmer
+  :init
+  (dimmer-mode)
+  (setq dimmer-fraction 0.3)
+  (dimmer-configure-which-key)
+  (dimmer-configure-magit))
+
+;; Zoom
+;; (use-package! zoom
+;;   :init
+;;   (zoom-mode t))
+;; (custom-set-variables
+;;  '(zoom-ignored-major-modes '(which-key-mode)))
+
+;; Beacon
+(use-package! beacon
+  :init
+  (beacon-mode t))
+
+;; Golden Ratio
+;; (use-package! golden-ratio
+;;   :init
+;;   (golden-ratio-mode 1)
+;;   (setq golden-ratio-auto-scale 1))
+
+;; Org Modern
+(after! org
+  (global-org-modern-mode))
+
+;; Org Super Agenda
+(after! org
+  (setq org-super-agenda-mode t))
