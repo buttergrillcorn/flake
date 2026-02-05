de263d610b553bc395138edff2a60c79a6228c8e
Yu Li (James) <jamesyuli@outlook.com>
2026-02-05 23:27:58 +0000

    (fix) home manager setup

--- nixos.nix
+++ nixos.nix
@@ -1,14 +1,16 @@
 { inputs, ... }: {
-
   flake = {
-
     nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
-        ./home.nix
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.james = import ./home.nix;
+        }
       ];
     };
-
   };
 
 }
