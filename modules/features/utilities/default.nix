a1e082e525f11259dffe35ede3794eb58116073d
Yu Li (James) <jamesyuli@outlook.com>
2026-05-08 13:23:33 +0100

    fix: remove Thunar from feature/utilities

--- modules/features/utilities/default.nix
+++ modules/features/utilities/default.nix
@@ -13,21 +13,6 @@
     yazi
   ];
 
-  # --- File Management (Thunar) ---
-  programs.thunar = {
-    enable = true;
-    plugins = with pkgs; [
-      thunar-archive-plugin
-      thunar-volman
-      thunar-vcs-plugin
-      thunar-shares-plugin
-      thunar-media-tags-plugin
-      ffmpegthumbnailer # Video thumbnail
-    ];
-  };
-  services.gvfs.enable = true; # For trash, mounting, etc.
-  services.tumbler.enable = true; # For image thumbnails.
-
   # --- External Storage ---
   services.udiskie.enable = true;
 
