77943c768d401911f13ff67a2f9a1caff97fb22c
Yu Li (James) <jamesyuli@outlook.com>
2026-05-14 18:16:09 +0100

    feat(qutebrowser): PicviewerCE+ and SponsorBlock

--- modules/features/browser/qutebrowser/default.nix
+++ modules/features/browser/qutebrowser/default.nix
@@ -16,5 +16,17 @@
       content.local_content_can_access_file_urls = true;
       content.local_content_can_access_remote_urls = true;
     };
+    greasemonkey = [
+      # Picture Viewer
+      (pkgs.fetchurl {
+        url = "https://hoothin.github.io/UserScripts/Picviewer%20CE+/dist.user.js";
+        sha256 = "sha256-kvqJaffGmIMRktnChZfj4ZjIJZOVvBwZx/9Jz5+yFU8=";
+      })
+      # Sponsor Block
+      (pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/mchangrh/sb.js/main/docs/sb-loader.user.js";
+        sha256 = "sha256-Y/On1rrQgqj5Zr9LazvUGIr67qZcwKLbn73odGRdsu8=";
+      })
+    ];
   };
 }
